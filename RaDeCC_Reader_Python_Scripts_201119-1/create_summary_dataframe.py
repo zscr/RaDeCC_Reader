7648b1c272261da9d24907bab10fcf6f21c01bab
Sean Selzer <seanselzer@googlemail.com>
2020-04-30 18:34:04 +0100

    Added Calculations to summary_df
    Added:
    228Th
    227Ac
    228Th_err
    227Ac_err

--- RaDeCC_Reader_Python_Scripts_201119-1/create_summary_dataframe.py
+++ RaDeCC_Reader_Python_Scripts_201119-1/create_summary_dataframe.py
@@ -8,19 +8,22 @@ Created on Tue Apr 21 11:59:11 2020
 
 import pandas as pd
 import numpy as np
+import copy
 
 
 
 def create_summary_dataframe(lvl2_main_df, log_df, sample_variable, sub_sample_variable):
     
     
-    summary_df = log_df
+    summary_df = copy.deepcopy(log_df)
     
     xs224_list = []
     xs223_list = []
     ra228_list = []
     th228_list = []
+    th228_err_list = []
     ac227_list = []
+    ac227_err_list = []
     error_list = []
     
     for index, row in summary_df.iterrows():
@@ -45,35 +48,95 @@ def create_summary_dataframe(lvl2_main_df, log_df, sample_variable, sub_sample_v
                     & (lvl2_main_df[sub_sample_variable]==row_sub_sample_variable)
                     & (lvl2_main_df['read_number']==1))
                     , 'vdpm224 (dpm/m^3)']
+            read1_vdpm224_err = lvl2_main_df.loc[((lvl2_main_df[sample_variable]==row_sample_variable) 
+                    & (lvl2_main_df[sub_sample_variable]==row_sub_sample_variable)
+                    & (lvl2_main_df['read_number']==1))
+                    , 'vdpm224_err (dpm/m^3)']
+            
             read3_vdpm224 = lvl2_main_df.loc[((lvl2_main_df[sample_variable]==row_sample_variable) 
                     & (lvl2_main_df[sub_sample_variable]==row_sub_sample_variable)
                     & (lvl2_main_df['read_number']==3))
                     , 'vdpm224 (dpm/m^3)']
-            
-            
-            if len(read1_vdpm224)>1 or len(read3_vdpm224)>1:
-                row_specific_errors.append('multi_reads_averaged')
+            read3_vdpm224_err = lvl2_main_df.loc[((lvl2_main_df[sample_variable]==row_sample_variable) 
+                    & (lvl2_main_df[sub_sample_variable]==row_sub_sample_variable)
+                    & (lvl2_main_df['read_number']==3))
+                    , 'vdpm224_err (dpm/m^3)']
+
+
+            if len(read1_vdpm224)>1 and len(read3_vdpm224)<1:
+                row_specific_errors.append('(224xs) multiple_1st_reads_averaged')
+                xs224_list.append(np.average(read1_vdpm224)-read3_vdpm224.iloc[0])
+                th228_list.append(read3_vdpm224.iloc[0])
+                th228_err_list.append(read3_vdpm224_err.iloc[0])
+                
+            if len(read1_vdpm224)<1 and len(read3_vdpm224)>1:
+                row_specific_errors.append('(224xs, 228Th) multiple_3rd_reads_averaged')
+                xs224_list.append(read1_vdpm224.iloc[0]-np.average(read3_vdpm224))
+                th228_list.append(np.average(read3_vdpm224))
+                th228_err_list.append(np.average(read3_vdpm224_err))
+                
+            if len(read1_vdpm224)>1 and len(read3_vdpm224)>1:
+                row_specific_errors.append('(224xs, 228Th) multiple_1st_and_3rd_reads_averaged')
                 xs224_list.append(np.average(read1_vdpm224)-np.average(read3_vdpm224))
+                th228_list.append(np.average(read3_vdpm224))
+                th228_err_list.append(np.average(read3_vdpm224_err))
+                
             else:
                 xs224_list.append(read1_vdpm224.iloc[0]-read3_vdpm224.iloc[0])
-            
+                th228_list.append(read3_vdpm224.iloc[0])
+                th228_err_list.append(read3_vdpm224_err.iloc[0])
+                
 #####   Read1 - Read4      
         elif 1 in read_number_set and 4 in read_number_set:
-            row_specific_errors.append('(224xs) using read 4 instead of 3')
+            row_specific_errors.append('(224xs, 228Th) using read 4 instead of 3')
+            
+            read1_vdpm224 = lvl2_main_df.loc[((lvl2_main_df[sample_variable]==row_sample_variable) 
+                    & (lvl2_main_df[sub_sample_variable]==row_sub_sample_variable)
+                    & (lvl2_main_df['read_number']==1))
+                    , 'vdpm224 (dpm/m^3)']
+            read1_vdpm224_err = lvl2_main_df.loc[((lvl2_main_df[sample_variable]==row_sample_variable) 
+                    & (lvl2_main_df[sub_sample_variable]==row_sub_sample_variable)
+                    & (lvl2_main_df['read_number']==1))
+                    , 'vdpm224_err (dpm/m^3)']
+            
             read4_vdpm224 = lvl2_main_df.loc[((lvl2_main_df[sample_variable]==row_sample_variable) 
                     & (lvl2_main_df[sub_sample_variable]==row_sub_sample_variable)
                     & (lvl2_main_df['read_number']==4))
                     , 'vdpm224 (dpm/m^3)']
+            read4_vdpm224_err = lvl2_main_df.loc[((lvl2_main_df[sample_variable]==row_sample_variable) 
+                    & (lvl2_main_df[sub_sample_variable]==row_sub_sample_variable)
+                    & (lvl2_main_df['read_number']==4))
+                    , 'vdpm224_err (dpm/m^3)']
             
-            if len(read1_vdpm224)>1 or len(read4_vdpm224)>1:
-                row_specific_errors.append('(xs224) multi_reads_averaged')
+            if len(read1_vdpm224)>1 and len(read4_vdpm224)<1:
+                row_specific_errors.append('(224xs) multiple_1st_reads_averaged')
+                xs224_list.append(np.average(read1_vdpm224)-read4_vdpm224.iloc[0])
+                th228_list.append(read4_vdpm224.iloc[0])
+                th228_err_list.append(read4_vdpm224.iloc[0])
+                
+            if len(read1_vdpm224)<1 and len(read4_vdpm224)>1:
+                row_specific_errors.append('(224xs, 228Th) multiple_4th_reads_averaged')
+                xs224_list.append(read1_vdpm224.iloc[0]-np.average(read4_vdpm224))
+                th228_list.append(np.average(read4_vdpm224))
+                th228_err_list.append(np.average(read4_vdpm224_err))
+                
+            if len(read1_vdpm224)>1 and len(read4_vdpm224)>1:
+                row_specific_errors.append('(224xs, 228Th) multiple_1st_and_4th_reads_averaged')
                 xs224_list.append(np.average(read1_vdpm224)-np.average(read4_vdpm224))
+                th228_list.append(np.average(read4_vdpm224))
+                th228_err_list.append(np.average(read4_vdpm224_err))
+                
             else:
                 xs224_list.append(read1_vdpm224.iloc[0]-read4_vdpm224.iloc[0])
-            
+                th228_list.append(read4_vdpm224.iloc[0])
+                th228_err_list.append(read4_vdpm224_err.iloc[0])
 #####   Reads missing             
         else:
-            xs224_list.append('(224xs) Required reads not available')
+            row_specific_errors.append('(224xs, 228Th) Required reads not available')
+            xs224_list.append(-999)
+            th228_list.append(-999)
+            th228_err_list.append(-999)
+            
         
 ###########################################################################################################################################
 ###########################################################################################################################################
@@ -86,18 +149,43 @@ def create_summary_dataframe(lvl2_main_df, log_df, sample_variable, sub_sample_v
                     & (lvl2_main_df[sub_sample_variable]==row_sub_sample_variable)
                     & (lvl2_main_df['read_number']==2))
                     , 'vdpm223 (dpm/m^3)']
+            read2_vdpm223_err = lvl2_main_df.loc[((lvl2_main_df[sample_variable]==row_sample_variable) 
+                    & (lvl2_main_df[sub_sample_variable]==row_sub_sample_variable)
+                    & (lvl2_main_df['read_number']==2))
+                    , 'vdpm223_err (dpm/m^3)']
+            
             read4_vdpm223 = lvl2_main_df.loc[((lvl2_main_df[sample_variable]==row_sample_variable) 
                     & (lvl2_main_df[sub_sample_variable]==row_sub_sample_variable)
                     & (lvl2_main_df['read_number']==4))
                     , 'vdpm223 (dpm/m^3)']
+            read4_vdpm223_err = lvl2_main_df.loc[((lvl2_main_df[sample_variable]==row_sample_variable) 
+                    & (lvl2_main_df[sub_sample_variable]==row_sub_sample_variable)
+                    & (lvl2_main_df['read_number']==4))
+                    , 'vdpm223_err (dpm/m^3)']
             
             
-            if len(read2_vdpm223)>1 or len(read4_vdpm223)>1:
-                row_specific_errors.append('(xs223) multi_reads_averaged')
+            if len(read2_vdpm223)<1 and len(read4_vdpm223)>1:
+                row_specific_errors.append('(xs223, 227Ac) multiple_4th_reads_averaged')
+                xs223_list.append(read2_vdpm223.iloc[0]-np.average(read4_vdpm223))
+                ac227_list.append(np.average(read4_vdpm223))
+                ac227_err_list.append(np.average(read4_vdpm223_err))
+                
+            if len(read2_vdpm223)>1 and len(read4_vdpm223)<1:
+                row_specific_errors.append('(xs223) multiple_2nd_reads_averaged')
+                xs223_list.append(np.average(read2_vdpm223)-read4_vdpm223.iloc[0])
+                ac227_list.append(read4_vdpm223.iloc[0])
+                ac227_err_list.append(read4_vdpm223_err.iloc[0])
+                
+            if len(read2_vdpm223)>1 and len(read4_vdpm223)>1:
+                row_specific_errors.append('(xs223, 227Ac) multiple_2nd_and_4th_reads_averaged')
                 xs223_list.append(np.average(read2_vdpm223)-np.average(read4_vdpm223))
+                ac227_list.append(np.average(read4_vdpm223))
+                ac227_err_list.append(np.average(read4_vdpm223_err))
+                
             else:
                 xs223_list.append(read2_vdpm223.iloc[0]-read4_vdpm223.iloc[0])
-        
+                ac227_list.append(read4_vdpm223.iloc[0])
+                ac227_err_list.append(read4_vdpm223_err.iloc[0])
         
         
         
@@ -105,28 +193,59 @@ def create_summary_dataframe(lvl2_main_df, log_df, sample_variable, sub_sample_v
         elif 1 in read_number_set and 4 in read_number_set:
 #            xs223_list.append('(223xs) using read 1 instead of 2')
             row_specific_errors.append('(223xs) using read 1 instead of 2')
+            
             read1_vdpm223 = lvl2_main_df.loc[((lvl2_main_df[sample_variable]==row_sample_variable) 
                     & (lvl2_main_df[sub_sample_variable]==row_sub_sample_variable)
                     & (lvl2_main_df['read_number']==1))
                     , 'vdpm223 (dpm/m^3)']
+            read1_vdpm223_err = lvl2_main_df.loc[((lvl2_main_df[sample_variable]==row_sample_variable) 
+                    & (lvl2_main_df[sub_sample_variable]==row_sub_sample_variable)
+                    & (lvl2_main_df['read_number']==1))
+                    , 'vdpm223_err (dpm/m^3)']
+            
             read4_vdpm223 = lvl2_main_df.loc[((lvl2_main_df[sample_variable]==row_sample_variable) 
                     & (lvl2_main_df[sub_sample_variable]==row_sub_sample_variable)
                     & (lvl2_main_df['read_number']==4))
                     , 'vdpm223 (dpm/m^3)']
+            read4_vdpm223_err = lvl2_main_df.loc[((lvl2_main_df[sample_variable]==row_sample_variable) 
+                    & (lvl2_main_df[sub_sample_variable]==row_sub_sample_variable)
+                    & (lvl2_main_df['read_number']==4))
+                    , 'vdpm223_err (dpm/m^3)']
             
-            if len(read1_vdpm223)>1 or len(read4_vdpm223)>1:
-                row_specific_errors.append('(xs223) multi_reads_averaged')
+
+            if len(read1_vdpm223)<1 and len(read4_vdpm223)>1:
+                row_specific_errors.append('(xs223, 227Ac) multiple_4th_reads_averaged')
+                xs223_list.append(read1_vdpm223.iloc[0]-np.average(read4_vdpm223))
+                ac227_list.append(np.average(read4_vdpm223))
+                ac227_err_list.append(np.average(read4_vdpm223_err))
+                
+            if len(read1_vdpm223)>1 and len(read4_vdpm223)<1:
+                row_specific_errors.append('(xs223) multiple_1st_reads_averaged')
+                xs223_list.append(np.average(read1_vdpm223)-read4_vdpm223.iloc[0])
+                ac227_list.append(read4_vdpm223.iloc[0])
+                ac227_err_list.append(read4_vdpm223_err.iloc[0])
+                
+            if len(read1_vdpm223)>1 and len(read4_vdpm223)>1:
+                row_specific_errors.append('(xs223, 227Ac) multiple_1st_and_4th_reads_averaged')
                 xs223_list.append(np.average(read1_vdpm223)-np.average(read4_vdpm223))
+                ac227_list.append(np.average(read4_vdpm223))
+                ac227_err_list.append(np.average(read4_vdpm223_err))
+                
             else:
                 xs223_list.append(read1_vdpm223.iloc[0]-read4_vdpm223.iloc[0])
+                ac227_list.append(read4_vdpm223.iloc[0])
+                ac227_err_list.append(read4_vdpm223_err.iloc[0])
 
 #####   Reads missing                
         else:
-            xs223_list.append('(223xs) Required reads not available')
+            row_specific_errors.append('(223xs,227Ac) Required reads not available')
+            ac227_list.append(-999)
+            ac227_err_list.append(-999)
+            xs223_list.append(-999)
         
 ###########################################################################################################################################
 ###########################################################################################################################################
-#       228Ra calculations      
+#       228Ra calculations    (under construction)  
         
         if 4 in read_number_set and 5 in read_number_set:
             ra228_list.append('228Ra is go')
@@ -137,16 +256,21 @@ def create_summary_dataframe(lvl2_main_df, log_df, sample_variable, sub_sample_v
 ###########################################################################################################################################
         error_list.append(row_specific_errors)
         
-#        print ('\n')
-        
     summary_df['224xs'] = xs224_list
+    summary_df['228Th'] = th228_list
+    summary_df['228Th_err'] = th228_err_list
     summary_df['223xs'] = xs223_list
+    summary_df['227Ac'] = ac227_list
+    summary_df['227Ac_err'] = ac227_err_list
     summary_df['228Ra'] = ra228_list
     summary_df['error_list'] = error_list
+    
+    cols = list(log_df.columns) + ['224xs',	'228Th',	 '228Th_err', '223xs', '227Ac',	'227Ac_err', '228Ra', 'error_list' ]
+    summary_df = summary_df[cols]
     
     summary_df.to_csv('/Users/seanselzer/Documents/GitHub/RaDeCC_Reader/Example_Output_Folder/Dataframes/summary_df.csv')
     
     
     return(summary_df)
 
-#print (create_summary_dataframe(lvl2_main_df, log_df, sample_variable, sub_sample_variable))
+print (create_summary_dataframe(lvl2_main_df, log_df, sample_variable, sub_sample_variable))
